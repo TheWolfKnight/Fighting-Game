0ff2df4ec4ed316a9dfab63d8d1dbdbf6e8af5f6
TheWolfKnight <philipalexknudsen@gmail.com>
2022-04-18 21:29:02 +0200

    commit #2

--- src/Entety/Collision.js
+++ src/Entety/Collision.js
@@ -6,14 +6,29 @@ function collide(attackEnt) {
         if (ent === attackEnt) {
             continue;
         }
-        if (attackEnt.position.x >= ent.position.x && attackEnt.position.x <= ent.position.x + ent.bounding.width
-            && attackEnt.position.y >= ent.position.y && attackEnt.position.y <= ent.position.y + ent.bounding.height) {
+
+        const AttackHalfWidth = attackEnt.rendering.bounding.width/2;
+        const AttackHalfHeight = attackEnt.rendering.bounding.heigth/2;
+
+        const EntHalfWidth = ent.rendering.bounding.width/2;
+        const EntHalfHeight = ent.rendering.bounding.heigth/2;
+
+        var distX = Math.abs((attackEnt.rendering.position.x + AttackHalfWidth) - (ent.rendering.position.x + EntHalfWidth));
+        var distY = Math.abs((attackEnt.rendering.position.y + AttackHalfHeight) - (ent.rendering.position.y + EntHalfHeight));
+
+        if ( distX <= AttackHalfWidth + EntHalfWidth && distY <= AttackHalfHeight + EntHalfHeight ) {
             return {status: true, collision: ent};
         }
-        if (attackEnt.position.x + attackEnt.bounding.width >= ent.position.x && attackEnt.position.x + attackEnt.bounding.width <= ent.position.x + ent.bounding.width
-            && attackEnt.position.y + attackEnt.bounding.height >= ent.position.y && attackEnt.position.y + attackEnt.bounding.height <= ent.position.y + ent.bounding.height) {
-                return {status: true, collision: ent};
-            }
+
+        // return if new idéer does not work
+        // if (attackEnt.position.x >= ent.position.x && attackEnt.position.x <= ent.position.x + ent.bounding.width
+        //     && attackEnt.position.y >= ent.position.y && attackEnt.position.y <= ent.position.y + ent.bounding.height) {
+        //     return {status: true, collision: ent};
+        // }
+        // if (attackEnt.position.x + attackEnt.bounding.width >= ent.position.x && attackEnt.position.x + attackEnt.bounding.width <= ent.position.x + ent.bounding.width
+        //     && attackEnt.position.y + attackEnt.bounding.height >= ent.position.y && attackEnt.position.y + attackEnt.bounding.height <= ent.position.y + ent.bounding.height) {
+        //         return {status: true, collision: ent};
+        //     }
     }
     return {status: false, collision: null};
 }
